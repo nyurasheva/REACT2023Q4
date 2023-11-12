d81452d46f7093820fcb9f05d8db35c407065e07
«nyurasheva» <nyurasheva@gmail.com>
2023-11-12 21:42:50 +0300

    feat: add and configure jest

---
+++ src/__tests__/NotFound.test.tsx
@@ -0,0 +1,25 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { NotFound } from '../pages/NotFound';
+
+describe('NotFound component', () => {
+  it('renders correctly', () => {
+    render(
+      <Router>
+        <NotFound />
+      </Router>
+    );
+
+    // Проверяем, что элементы находятся на странице
+    expect(screen.getByText('404')).toBeTruthy();
+    expect(screen.getByText('Ошибка')).toBeTruthy();
+    expect(
+      screen.getByText('Извините! Мы не смогли найти то, что вы искали.')
+    ).toBeTruthy();
+    expect(screen.getByText('Запрашиваемая страница не найдена.')).toBeTruthy();
+    expect(
+      screen.getByRole('link', { name: 'Перейти на главную страницу' })
+    ).toBeTruthy();
+  });
+});
